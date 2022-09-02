653a2e6d28a232380b03c141fd7f7eef63ac196c
shakib04 <mdshakibulalam@gmail.com>
2022-09-02 11:29:44 +0600

    remove unused index component from app-module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { IndexComponent } from './programiz/index/index.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    IndexComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
