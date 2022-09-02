edfc4f20e34f205225d4dbacc34f7d01ee1c88d3
shakibul <shakibul.alam@bracits.com>
2022-09-02 11:25:40 +0600

    fresh project to enable debuggin

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { IndexComponent } from './programiz/index/index.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    IndexComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
